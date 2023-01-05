05d597332feefad93bedefce21e4fbba14a63fac
Lans12345 <olanalans12345@gmail.com>
2023-01-05 18:22:18 +0800

    initial commit

---
+++ android/app/src/main/kotlin/com/example/files_tracking/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.files_tracking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/appbar_widget.dart
@@ -0,0 +1,14 @@
+import 'package:files_tracking/widgets/text_widget.dart';
+import 'package:flutter/material.dart';
+
+PreferredSizeWidget AppbarWidget(
+  String title,
+) {
+  return AppBar(
+    elevation: 0,
+    foregroundColor: Colors.black,
+    backgroundColor: Colors.white,
+    title: TextRegular(text: title, fontSize: 18, color: Colors.black),
+    centerTitle: true,
+  );
+}
